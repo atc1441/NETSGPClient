5fc2908b7b5840f771b4629758138eb2d7d4f373
Moritz Wirger <lj.moritorii@web.de>
2022-08-15 12:56:22 +0200

    Use macros to use different Serials on different architectures
    - closes #19

--- examples/PollDemo/PollDemo.ino
+++ examples/PollDemo/PollDemo.ino
@@ -1,24 +1,39 @@
 #include "NETSGPClient.h"
 
 constexpr const uint8_t PROG_PIN = 4; /// Programming enable pin of RF module
-constexpr const uint8_t RX_PIN = 16; /// RX pin of ESP connect to TX of RF module
-constexpr const uint8_t TX_PIN = 17; /// TX pin of ESP connect to RX of RF module
+constexpr const uint8_t RX_PIN = 16; /// RX pin of ESP32 connect to TX of RF module
+constexpr const uint8_t TX_PIN = 17; /// TX pin of ESP32 connect to RX of RF module
 constexpr const uint32_t inverterID = 0x11002793; /// Identifier of your inverter (see label on inverter)
 
-NETSGPClient client(Serial2, PROG_PIN); /// NETSGPClient instance
+#if defined(ESP32)
+// On ESP32 debug output is on Serial and RF module connects to Serial2
+#define debugSerial Serial
+#define clientSerial Serial2
+#else
+// On ESP8266 or other debug output is on Serial1 and RF module connects to Serial
+// On D1 Mini connect RF module to pins marked RX and TX and use D4 for debug output
+#define debugSerial Serial1
+#define clientSerial Serial
+#endif
+
+NETSGPClient client(clientSerial, PROG_PIN); /// NETSGPClient instance
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial2.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+    debugSerial.begin(115200);
+if defined(ESP32)
+    clientSerial.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+#else
+    clientSerial.begin(9600);
+#endif
     pinMode(LED_BUILTIN, OUTPUT);
     delay(1000);
-    Serial.println("Welcome to Micro Inverter Interface by ATCnetz.de and enwi.one");
+    debugSerial.println("Welcome to Micro Inverter Interface by ATCnetz.de and enwi.one");
 
     // Make sure the RF module is set to the correct settings
     if (!client.setDefaultRFSettings())
     {
-        Serial.println("Could not set RF module to default settings");
+        debugSerial.println("Could not set RF module to default settings");
     }
 }
 
@@ -31,25 +46,25 @@ void loop()
         lastSendMillis = currentMillis;
 
         digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-        Serial.println("");
-        Serial.println("Sending request now");
+        debugSerial.println("");
+        debugSerial.println("Sending request now");
 
         const NETSGPClient::InverterStatus status = client.getStatus(inverterID);
         if (status.valid)
         {
-            Serial.println("*********************************************");
-            Serial.println("Received Inverter Status");
-            Serial.print("Device: ");
-            Serial.println(status.deviceID, HEX);
-            Serial.println("Status: " + String(status.state));
-            Serial.println("DC_Voltage: " + String(status.dcVoltage) + "V");
-            Serial.println("DC_Current: " + String(status.dcCurrent) + "A");
-            Serial.println("DC_Power: " + String(status.dcPower) + "W");
-            Serial.println("AC_Voltage: " + String(status.acVoltage) + "V");
-            Serial.println("AC_Current: " + String(status.acCurrent) + "A");
-            Serial.println("AC_Power: " + String(status.acPower) + "W");
-            Serial.println("Power gen total: " + String(status.totalGeneratedPower));
-            Serial.println("Temperature: " + String(status.temperature));
+            debugSerial.println("*********************************************");
+            debugSerial.println("Received Inverter Status");
+            debugSerial.print("Device: ");
+            debugSerial.println(status.deviceID, HEX);
+            debugSerial.println("Status: " + String(status.state));
+            debugSerial.println("DC_Voltage: " + String(status.dcVoltage) + "V");
+            debugSerial.println("DC_Current: " + String(status.dcCurrent) + "A");
+            debugSerial.println("DC_Power: " + String(status.dcPower) + "W");
+            debugSerial.println("AC_Voltage: " + String(status.acVoltage) + "V");
+            debugSerial.println("AC_Current: " + String(status.acCurrent) + "A");
+            debugSerial.println("AC_Power: " + String(status.acPower) + "W");
+            debugSerial.println("Power gen total: " + String(status.totalGeneratedPower));
+            debugSerial.println("Temperature: " + String(status.temperature));
         }
     }
 }
